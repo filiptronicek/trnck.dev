149bc72be563d4904bf6bc197e372664f53a231b
Filip Troníček <filip.tronicek@seznam.cz>
2020-03-30 18:34:25 +0200

    Names update

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,9 @@ const names = [
   "blogger",
   "team leader",
   "memer",
-  "Redditor"
+  "Redditor",
+  "perfectionist",
+  "wannabe security expert"
 ];
 
 function sleep(ms) {
@@ -23,18 +25,43 @@ function sleep(ms) {
 }
 
 async function changeText() {
-    const randomText = names[Math.floor(Math.random() * names.length)];
-  const randomArray = randomText.split("");
-  if (changingEl.innerText !== randomText) {
-    changingEl.innerText = "";
-
-    for (let character of randomArray) {
-      changingEl.innerText += character;
-      await sleep(200);
+  const randomText = names[Math.floor(Math.random() * names.length)];
+  if (randomText) {
+    const randomArray = randomText.split("");
+    if (changingEl.innerText !== randomText && names.length > 0) {
+      const index = names.indexOf(randomText);
+      if (index > -1) {
+        names.splice(index, 1);
+      }
+      console.log(names);
+      changingEl.innerText = "";
+      for (let character of randomArray) {
+        changingEl.innerText += character;
+        await sleep(200);
+      }
+    } else {
+      changingEl.innerText = "";
+      for (let character of "web developer") {
+        changingEl.innerText += character;
+        await sleep(200);
+      }
     }
   }
   await sleep(1000);
 
   changeText();
 }
+
 setTimeout(changeText, 1000);
+
+/* Smooth scrolling */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
