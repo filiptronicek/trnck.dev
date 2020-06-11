359428a6de113a9d990cb267dc1cdada4420bd32
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-11 23:45:38 +0200

    Add 404

---
+++ assets/js/404.js
@@ -0,0 +1,16 @@
+const jokeDiv = document.getElementById("joke");
+
+function get_joke_of_the_day() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+	 if (this.readyState == 4 && this.status == 200) {
+         jokeDiv.innerText = JSON.parse(this.responseText).setup;
+         jokeDiv.innerHTML += `<br><span class="punchline">${JSON.parse(this.responseText).punchline}</span>`;
+	 }
+    };
+    xhttp.open("GET", "https://cors-anywhere.herokuapp.com/official-joke-api.appspot.com/jokes/random", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+}
+
+get_joke_of_the_day();
