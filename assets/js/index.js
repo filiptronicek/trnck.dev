e8a6f701c6adf94ac39fd170402026ddd2e0ed63
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-13 17:34:32 +0200

    Update to v2 of API

--- assets/js/index.js
+++ assets/js/index.js
@@ -64,8 +64,8 @@ function getRank() {
   fetch(url)
     .then((response) => response.json())
     .then((res) => {
-      for (const user of res.users) {
-        if (user.user.username === me.username) {
+      for (const user of res.users.users) {
+        if (user.login === me.username) {
           rankDiv.innerText = `${user.rank}${
             user.rank === 1 ? "st" : user.rank === 2 ? "nd" : "th"
           }`;
