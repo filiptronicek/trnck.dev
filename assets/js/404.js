3519e6e107a37a1076d884a255deb8058397b799
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-13 15:03:44 +0200

    Camelcase

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,6 +1,6 @@
 const jokeDiv = document.getElementById("joke");
 
-function get_joke_of_the_day() {
+function getJokeOfTheDay() {
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
