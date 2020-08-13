af20ca38a3034322b5ce0932e87bc7c40389ef5d
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-13 13:24:17 +0200

    Serverless function

---
+++ api/commit.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch');
+
+module.exports = async (_req, res) => {
+    await fetch(
+        "https://api.github.com/repos/filiptronicek/trnck.dev/commits"
+      ).then((data) => data.json()).then((data) => {
+        res.send(data);
+      });
+};
