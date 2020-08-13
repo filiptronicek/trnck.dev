30c5a3fb50199f0d2564ad6d5466afda0232ffe7
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-13 15:17:50 +0200

    Triple ===

--- assets/js/404.js
+++ assets/js/404.js
@@ -3,7 +3,7 @@ const jokeDiv = document.getElementById("joke");
 function getJokeOfTheDay() {
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState === 4 && this.status === 200) {
       jokeDiv.innerText = JSON.parse(this.responseText).setup;
       jokeDiv.innerHTML += `<br><span class="punchline">${
         JSON.parse(this.responseText).punchline
