b0eee7d01078b2685ce56335bc8fa7b030af9d73
Restyled.io <commits@restyled.io>
2020-08-13 13:22:15 +0000

    Restyled by clang-format

--- assets/js/404.js
+++ assets/js/404.js
@@ -2,18 +2,17 @@ const jokeDiv = document.getElementById("joke");
 
 function getJokeOfTheDay() {
   const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
+  xhttp.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
       jokeDiv.innerText = JSON.parse(this.responseText).setup;
       jokeDiv.innerHTML += `<br><span class="punchline">${
-        JSON.parse(this.responseText).punchline
-      }</span>`;
+          JSON.parse(this.responseText).punchline}</span>`;
     }
   };
   xhttp.open(
-    "GET",
-    "https://cors-anywhere.herokuapp.com/official-joke-api.appspot.com/jokes/random",
-    true,
+      "GET",
+      "https://cors-anywhere.herokuapp.com/official-joke-api.appspot.com/jokes/random",
+      true,
   );
   xhttp.setRequestHeader("Content-type", "application/json");
   xhttp.send();
