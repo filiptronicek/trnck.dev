24361c7fa1561822309ac0efd371e7d2f7d10df6
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-13 13:30:40 +0200

    Update commit.js

--- api/commit.js
+++ api/commit.js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-module.exports = async(_req, res) => {
+export default async(_req, res) => {
     await fetch(
         "https://api.github.com/repos/filiptronicek/trnck.dev/commits"
       ).then((data) => data.json()).then((data) => {
