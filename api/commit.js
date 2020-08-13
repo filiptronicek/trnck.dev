f5fe524286cf1f1e646cecc08c65392273d204eb
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-13 13:27:15 +0200

    Delete whitespace

--- api/commit.js
+++ api/commit.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-module.exports = async (_req, res) => {
+module.exports = async(_req, res) => {
     await fetch(
         "https://api.github.com/repos/filiptronicek/trnck.dev/commits"
       ).then((data) => data.json()).then((data) => {
