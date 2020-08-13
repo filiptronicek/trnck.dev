8242794e6b30264f1935691b7bc94738d1b389e3
Restyled.io <commits@restyled.io>
2020-08-13 13:07:44 +0000

    Restyled by clang-format

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,10 @@ const sponsorsDiv = document.getElementById("sponsors");
 const commitSp = document.getElementById("commit");
 
 const me = {
-  username: "filiptronicek",
+  username : "filiptronicek",
 };
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+function sleep(ms) { return new Promise((resolve) => setTimeout(resolve, ms)); }
 
 async function changeText() {
   const randomText = names[Math.floor(Math.random() * names.length)];
@@ -42,39 +40,31 @@ async function changeText() {
 function getRank() {
   const url = "https://commiters.now.sh/rank/czech_republic";
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((res) => {
-      for (const user of res.users.users) {
-        if (user.login === me.username) {
-          rankDiv.innerText = `${user.rank}${
-            user.rank === 1 ? "st" : user.rank === 2 ? "nd" : "th"
-          }`;
-        }
+  fetch(url).then((response) => response.json()).then((res) => {
+    for (const user of res.users.users) {
+      if (user.login === me.username) {
+        rankDiv.innerText = `${user.rank}${
+            user.rank === 1 ? "st" : user.rank === 2 ? "nd" : "th"}`;
       }
-    });
+    }
+  });
 }
 function getSponsors() {
   const url = `https://sponsors.trnck.dev/${me.username}/count`;
-  fetch(url)
-    .then((responce) => responce.json())
-    .then((res) => {
-      const respData = res.sponsors;
-      sponsorsDiv.innerText = `${respData.count} sponsor${
-        respData.count === 1 ? "" : "s"
-      }`;
-    });
+  fetch(url).then((responce) => responce.json()).then((res) => {
+    const respData = res.sponsors;
+    sponsorsDiv.innerText =
+        `${respData.count} sponsor${respData.count === 1 ? "" : "s"}`;
+  });
 }
 
 const getCommit = () => {
   const url = `/api/commit`;
-  fetch(url)
-    .then((responce) => responce.json())
-    .then((res) => {
-      commitSp.innerHTML = `<a href="${
-        res[0].html_url
-      }" target="_blank" rel="noreferrer">${res[0].sha.substring(0, 7)}</a>`;
-    });
+  fetch(url).then((responce) => responce.json()).then((res) => {
+    commitSp.innerHTML =
+        `<a href="${res[0].html_url}" target="_blank" rel="noreferrer">${
+            res[0].sha.substring(0, 7)}</a>`;
+  });
 };
 getCommit();
 getSponsors();
@@ -85,11 +75,11 @@ setTimeout(changeText, 1000);
 /* Smooth scrolling */
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener("click", function(e) {
     e.preventDefault();
 
     document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
+      behavior : "smooth",
     });
   });
 });
