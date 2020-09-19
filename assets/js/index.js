1f8614375a0d2a85f37e57c931697ec5f94d8c74
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-19 16:51:11 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,7 @@ function formatBytes(bytes, decimals = 0) {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
 
-const vercel = [27721427, 7094694981];
+const vercel = [27672562, 7091123363];
 
 const sum = vercel.reduce((a, b) => a + b, 0);
 
