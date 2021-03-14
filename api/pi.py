2b2011ec34beea4c5a7a503d26d75c9df31ec9f2
Filip Troníček <filip.tronicek@seznam.cz>
2021-03-14 17:44:55 +0100

    Update pi.py

--- api/pi.py
+++ api/pi.py
@@ -59,5 +59,5 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(main())
+    self.wfile.write(main().encode())
     return
