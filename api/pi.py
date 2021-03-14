14bdd20d28a15ea20813c2c5b2a6d06bf720856f
Filip Troníček <filip.tronicek@seznam.cz>
2021-03-14 17:25:16 +0100

    Create pi.py

---
+++ api/pi.py
@@ -0,0 +1,54 @@
+def calcPi(limit):  # Generator function
+    """
+    Prints out the digits of PI
+    until it reaches the given limit
+    """
+
+    q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
+
+    decimal = limit
+    counter = 0
+
+    while counter != decimal + 1:
+            if 4 * q + r - t < n * t:
+                    # yield digit
+                    yield n
+                    # insert period after first digit
+                    if counter == 0:
+                            yield '.'
+                    # end
+                    if decimal == counter:
+                            print('')
+                            break
+                    counter += 1
+                    nr = 10 * (r - n * t)
+                    n = ((10 * (3 * q + r)) // t) - 10 * n
+                    q *= 10
+                    r = nr
+            else:
+                    nr = (2 * q + r) * l
+                    nn = (q * (7 * k) + 2 + (r * l)) // (t * l)
+                    q *= k
+                    t *= l
+                    l += 2
+                    k += 1
+                    n = nn
+                    r = nr
+
+
+def main():  # Wrapper function
+
+    # Calls CalcPi with the given limit
+    return calcPi(8000)
+
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(main())
+    return
+
