a0adf0f8bc46ff04c658bdfe983323d6cd4e6ffe
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-01 22:02:19 +0200

    Remove old stuff

--- api/commit.js
+++
@@ -1,3 +0,0 @@
-export default async (_req, res) => {
-  res.json({ data: process.env.VERCEL_GITHUB_COMMIT_SHA });
-};
--- api/contribs.js
+++
@@ -1,7 +0,0 @@
-const fetch = require('node-fetch');
-
-export default async (req, res) => {
-    const usr = req.query.username;
-    const url = `https://github-contributions.now.sh/api/v1/${usr}`;
-    fetch(url).then((responce) => responce.json()).then(r => res.json(r));
-};
